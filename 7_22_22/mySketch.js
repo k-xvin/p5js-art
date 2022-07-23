73a93232117f79f4c868e4fa7f9e9f13baf74d82
Kevin Xu <bobthehoboofhobos@gmail.com>
2022-07-23 00:40:15 -0700

    7_22_22

---
+++ 7_22_22/mySketch.js
@@ -0,0 +1,78 @@
+// OPC.toggle('blend_DODGE', false);
+// OPC.toggle('blend_BURN', false);
+
+OPC.button('blendButton', 'cycle blendmode');
+let blendCycle;
+
+//https://coolors.co/1e1e24-c19ab7-9c95dc-228cdb-0b7189
+let palette = ["#1e1e24","#c19ab7","#9c95dc","#228cdb","#0b7189"];
+let scaleFactor = 10;
+let t;
+let bgColor;
+let points = [];
+function setup() {
+	blendCycle = 0;
+	createCanvas(windowWidth, windowHeight);
+	strokeWeight(5);
+	stroke(palette[1]);
+	t = 0;
+	bgColor = color(palette[0]);
+	
+	noFill();
+	for(let i=0;i<500;i++){
+		// circle(random(50,300)*cos(t), random(50,300)*sin(t)+10*sin(t), 20);
+		let point = p5.Vector.random2D().mult(random(50,windowWidth));
+		let col = color(palette[floor(random(1,5))]);
+		col.setAlpha(200);
+		points[i] = {
+			"x": point.x, 
+			"y": point.y,
+			"timeOffset": random(0, PI),
+			"col": col
+		};
+	}
+	
+	background(bgColor);
+}
+
+function draw() {
+	if(blendCycle == 0){
+		blendMode(DODGE);
+	}
+	else if(blendCycle == 1){
+		blendMode(BURN);
+	}
+	else{
+		blendMode(NORMAL);
+		bgColor.setAlpha(30);
+		background(bgColor);
+	}
+	
+	
+	translate(windowWidth/2, windowHeight/2);
+	
+	t += 0.01;
+	for(let i=0;i<500;i++){
+		stroke(points[i].col);
+		let timeWithOffset = t + points[i].timeOffset;
+		point(points[i].x*cos(timeWithOffset)*sin(timeWithOffset)*cos(timeWithOffset), 
+					 points[i].y*sin(timeWithOffset)*cos(timeWithOffset)*sin(timeWithOffset), 
+					 );
+		
+	}
+	
+}
+
+function buttonPressed(variableName) {
+	if(variableName == 'blendButton'){
+		if(blendCycle < 2){
+			blendCycle++;
+			bgColor.setAlpha(255);
+			clear();
+		}
+		else {
+			blendCycle = 0;
+		}
+		console.log(blendCycle);
+	}
+}
